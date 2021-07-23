b748c538b2162f84d4014d3645aa66003b42a940
David <dleeljcds@gmail.com>
2021-07-22 19:55:52 -0700

    Lint Changes Formatted

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.jsx';
+// eslint-disable-next-line no-unused-vars
 import Main from './components/relatedProducts/Main.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
