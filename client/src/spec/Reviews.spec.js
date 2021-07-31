5b1e54ff3c5f51adb60b7a95063fc5876f49173e
Jaimie Diemer <pbandjaimie@gmail.com>
2021-07-31 12:32:31 -0700

    One valid Reviews test added

--- client/src/spec/Reviews.spec.js
+++ client/src/spec/Reviews.spec.js
@@ -1,16 +1,34 @@
 /* eslint-disable no-undef */
 import React from 'react';
+import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import App from '../app.jsx';
 import Reviews from '../components/Reviews/Reviews.jsx';
+import ReviewTiles from '../components/Reviews/ReviewTiles.jsx';
+import 'regenerator-runtime/runtime';
+import {act, render, fireEvent, waitFor, screen} from '@testing-library/react'
+import { unmountComponentAtNode } from "react-dom";
 
-describe('Reviews Component', () => {
-  test('Test that Reviews Component renders', () => {
-    const component = renderer.create(<Reviews ></Reviews>);
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+
+
+it('should render ReviewTiles component', () => {
+  act(() => {
+    let div = document.createElement('div');
+    ReactDOM.render(<ReviewTiles />, div);
+
+    let element = div.querySelector('.reviewTiles');
+    expect(element.innerHTML).toBe('<strong>!!!Review Tiles Here!!!</strong>');
   });
 });
+
+
+// describe('Reviews Component', () => {
+//   test('Test that Reviews Component renders', () => {
+//     const component = renderer.create(<Reviews ></Reviews>);
+//     let tree = component.toJSON();
+//     expect(tree).toMatchSnapshot();
+//   });
+// });
 
 
 
