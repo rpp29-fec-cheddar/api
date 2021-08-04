10d758a6a618793fcdafa4750b06ab3b8d1cc68d
Jaimie Diemer <pbandjaimie@gmail.com>
2021-08-03 23:06:29 -0700

    Revised star ratings for easier use

--- client/src/components/Reviews/Stars.jsx
+++ client/src/components/Reviews/Stars.jsx
@@ -17,51 +17,26 @@ let Stars = (props) => {
     averageRating = (totalOfRatings / howManyRatings);
     averageRating = Number((averageRating).toFixed(1));
   }
+  let ratingPercentage = averageRating * 20;
 
-  //applying average rating to stars
-  let ratingBefore = averageRating;
-  let rating = averageRating;
-
-  let stars = [];
-  while (stars.length < 5) {
-    if (rating > 1) {
-      stars.push(1);
-    } else if (rating > 0) {
-      if (rating >= 0.13 && rating <= 0.37) {
-        stars.push(0.37);
-      } else if (rating >= 0.38 && rating <= 0.62) {
-        stars.push(0.52);
-      } else if (rating >= 0.63 && rating <= 0.87) {
-        stars.push(0.7);
-      } else if (rating >= 0.88) {
-        stars.push(1)
-      } else {
-        stars.push(0);
-      }
-    } else {
-      stars.push(0);
-    }
-    rating = rating - 1;
-  }
 
   return (
     <div>
-      <strong>!!!Average Rating and Stars Here!!!</strong>
-      <h1>{ratingBefore}</h1>
-      <div>
-        {stars.map((item, i) => {
-          return (
-            <div className="star-box" key={i}>
-              <div className="star-filled-in" style={{'width': `${parseInt(item * 31)}px`}}>
-                <img className="star-outline" src="star.png" alt="stars alt"></img>
-              </div>
-            </div>
-          );
-        })}
+      <h1>{averageRating}</h1>
+      <div className='starContainer'>
+        <div className='starBox' style={{'width': `${ratingPercentage}%`}}>
+          <div className='inlineStars'>
+            <img className="starsLayout" src="star.png" alt="Star" />
+            <img className="starsLayout" src="star.png" alt="Star" />
+            <img className="starsLayout" src="star.png" alt="Star" />
+            <img className="starsLayout" src="star.png" alt="Star" />
+            <img className="starsLayout" src="star.png" alt="Star" />
+          </div>
+        </div>
       </div>
       <h4>{totalOfRatings} reviews</h4>
     </div>
-  );
-};
+  )
+}
 
-export default Stars;
+export default Stars
