2da68ac408087959f151dff97130e6d4dcdd958d
Abdulhameed Nouraldean <abdelhumayd@gmail.com>
2021-08-05 23:02:08 -0400

    update SearchQuestion

--- client/src/components/QandA/SearchQuestion.jsx
+++ client/src/components/QandA/SearchQuestion.jsx
@@ -7,7 +7,7 @@ const SearchQuestion = (props) => {
         <input type = 'text'
           placeholder='HAVE A QUESTION? SEARCH FOR ANSWERS...'
           onChange={props.cQuestion}
-          value={props.questionSearch}
+          value={props.qSearch}
           className='form-control form-group'
         />
         <input type='submit' className='btn btn-danger btn-block' value='Submit'></input>
