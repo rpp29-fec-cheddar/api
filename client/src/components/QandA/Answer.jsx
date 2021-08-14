f6f90dd2563cc5702466a017c850dbdc44589081
Abdulhameed Nouraldean <abdelhumayd@gmail.com>
2021-08-14 19:11:46 -0400

    Update Question and Answers to add formating

--- client/src/components/QandA/Answer.jsx
+++ client/src/components/QandA/Answer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Answer = (props) => {
   return (
     <div className="Answers">
-      <ul>{props.answerbody}</ul>
+      A: {props.answerbody}
     </div>
   )
 }
--- client/src/components/QandA/Question.jsx
+++ client/src/components/QandA/Question.jsx
@@ -4,10 +4,10 @@ import Answer from './Answer.jsx'
 const Question = (props) => {
   return (
     <div className="Questions">
-      <li>Q: {props.question}</li>
-      <ul>{Object.keys(props.answer).map((keyNames, index) =>
+      Q: {props.question}
+      {Object.keys(props.answer).map((keyNames, index) =>
         <Answer answerbody={props.answer[keyNames].body} key={index}/>
-      )}</ul>
+      )}
     </div>
   )
 }
