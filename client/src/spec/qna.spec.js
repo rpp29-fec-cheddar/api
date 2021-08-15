c7c4e3a8eb22c78c40538ea7766f8a3dd9f4ab98
Abdulhameed Nouraldean <abdelhumayd@gmail.com>
2021-08-15 14:49:34 -0400

    Install packages for test suite

--- client/src/spec/qna.spec.js
+++ client/src/spec/qna.spec.js
@@ -2,27 +2,44 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { expect } from 'chai';
 
+import QnA from '../components/QandA/QandA.jsx';
+import QuestionList from '../components/QandA/QuestionList.jsx';
 
+Enzyme.configure({ adapter: new Adapter() });
 
-describe('QandA Component Tests', () => {
+describe('QnA Component Tests', () => {
+  // it('It test the QnA component', () => {
+  //   const wrapper = shallow(QnA);
+  //   expect(wrapper.find('.qnAWrapper')).to.have.lengthOf(3);
+  // });
+  function Bar() {
+    return (
+      <div>
+        <div className="in-bar" />
+      </div>
+    );
+  }
+
+  function Foo() {
+    return (
+      <div>
+        <Bar />
+      </div>
+    );
+  }
+
+  it('test from docs', ()=> {
+    const wrapper = shallow(<Foo />);
+    expect(wrapper.find('.in-bar')).to.have.lengthOf(0);
+    expect(wrapper.find(Bar)).to.have.lengthOf(1);
+    expect(wrapper.find(Bar).shallow().find('.in-bar')).to.have.lengthOf(1);
+  })
+
 
 });
 
-
-// describe('My Test Suite', () => {
-//   // eslint-disable-next-line no-undef
-//   it('My Test Case', () => {
-//     expect(true).toEqual(true);
-//   });
-// });
-
-// // eslint-disable-next-line func-style
-// function sayHello (string) {
-//   let response = 'Hello ' + string + '!';
-//   return response;
-// }
-
-// test('responds with hello to input string', () => {
-//   expect(sayHello('David')).toBe('Hello David!');
-// })
