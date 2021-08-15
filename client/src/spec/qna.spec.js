65c5fbc13b7562d20731ceb5e5067ba390df0ff7
Abdulhameed Nouraldean <abdelhumayd@gmail.com>
2021-08-15 16:34:23 -0400

    Add unit test coverage for components

--- client/src/spec/qna.spec.js
+++ client/src/spec/qna.spec.js
@@ -2,44 +2,50 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import sinon from 'sinon';
+
+import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
 import QnA from '../components/QandA/QandA.jsx';
 import QuestionList from '../components/QandA/QuestionList.jsx';
+import SearchQuestion from '../components/QandA/SearchQuestion.jsx';
+import TwoButtons from '../components/QandA/TwoButtons.jsx'
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('QnA Component Tests', () => {
-  // it('It test the QnA component', () => {
-  //   const wrapper = shallow(QnA);
-  //   expect(wrapper.find('.qnAWrapper')).to.have.lengthOf(3);
-  // });
-  function Bar() {
-    return (
-      <div>
-        <div className="in-bar" />
-      </div>
-    );
-  }
+describe('QandA Component Tests', () => {
 
-  function Foo() {
-    return (
-      <div>
-        <Bar />
-      </div>
-    );
-  }
+  it('renders only 1 wrapper', () => {
+    let wrapper = shallow(<QnA />);
+    expect(wrapper.find('.qnAWrapper')).to.have.lengthOf(1);
+  });
 
-  it('test from docs', ()=> {
-    const wrapper = shallow(<Foo />);
-    expect(wrapper.find('.in-bar')).to.have.lengthOf(0);
-    expect(wrapper.find(Bar)).to.have.lengthOf(1);
-    expect(wrapper.find(Bar).shallow().find('.in-bar')).to.have.lengthOf(1);
-  })
+  it('renders a header "qHeader"', () => {
+    let wrapper = shallow(<QnA />);
+    expect(wrapper.find('.qHeader')).to.have.lengthOf(1);
+  });
 
+  it('renders the SearchQuestion, QuestionList, and TwoButtons component', () => {
+    let wrapper = shallow(<QnA />);
+    expect(wrapper.contains(<QuestionList />)).to.equal(true);
+    expect(wrapper.containsMatchingElement(<SearchQuestion />)).to.equal(true);
+    expect(wrapper.containsMatchingElement(<TwoButtons />)).to.equal(true);
+  });
+
+});
+
+describe('QuestionList Component Test', () => {
+
+  it('contains 1 "questionList" div', () => {
+
+    let data = [' ', ' ']
+    let wrapper = shallow(<QuestionList qAndA={data}/>);
+
+    expect(wrapper.find('.questionList')).to.have.lengthOf(1);
+  });
 
 });
 
