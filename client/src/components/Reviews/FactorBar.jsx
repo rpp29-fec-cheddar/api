01d06fa1b798d65e65a0ebcfc3c6c0e2a2be27f9
Jaimie Diemer <pbandjaimie@gmail.com>
2021-08-18 01:31:58 -0700

    Removed unnecessary center descriptor

--- client/src/components/Reviews/FactorBar.jsx
+++ client/src/components/Reviews/FactorBar.jsx
@@ -3,42 +3,35 @@ import React from 'react';
 const FactorBar = (props) => {
   let factorValue = (props.value / 5) * 13;
   let leftDescriptor,
-    centerDescriptor,
     rightDescriptor;
 
   if (props.name === 'Size') {
     leftDescriptor = 'A size too small';
-    centerDescriptor = 'Perfect';
     rightDescriptor = 'A size too large';
   }
 
   if (props.name === 'Width') {
     leftDescriptor = 'Too narrow';
-    centerDescriptor = 'Perfect';
     rightDescriptor = 'Too wide'
   }
 
   if (props.name === 'Comfort') {
     leftDescriptor = 'Uncomfortable';
-    centerDescriptor = 'Ok';
     rightDescriptor = 'Perfect'
   }
 
   if (props.name === 'Quality') {
     leftDescriptor = 'Poor';
-    centerDescriptor = 'What I expected';
     rightDescriptor = 'Perfect'
   }
 
   if (props.name === 'Length') {
     leftDescriptor = 'Runs short';
-    centerDescriptor = 'Perfect';
     rightDescriptor = 'Runs long';
   }
 
   if (props.name === 'Fit') {
     leftDescriptor = 'Runs tight';
-    centerDescriptor = 'Perfect';
     rightDescriptor = 'Runs loose';
   }
 
@@ -50,7 +43,6 @@ const FactorBar = (props) => {
           <div className="factorPoint" style={{'marginLeft': `${factorValue}em`}}></div>
           <div className="descriptors">
             <div className="leftDescriptor">{leftDescriptor}</div>
-            <div className="centerDescriptor">{centerDescriptor}</div>
             <div className="rightDescriptor">{rightDescriptor}</div>
           </div>
         </div>
